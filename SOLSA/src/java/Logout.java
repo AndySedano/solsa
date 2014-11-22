366f42cbbf2fb0b507baecb54c438cb5cc996361
Arturo Torres Sánchez <r2d2.art2005@gmail.com>
2014-11-22 13:29:45 -0600

    Ya se puede hacer Logout

--- SOLSA/src/java/Logout.java
+++ SOLSA/src/java/Logout.java
@@ -1,22 +1,20 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-public class Logout extends HttpServlet {
-
+public class Logout extends HttpServlet
+{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-       
-    }
-
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException
+    {
+        request .setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
         
+        HttpSession session = request.getSession();
+        session.setAttribute("username", null);
+        session.setAttribute("tipo", null);
+        
+        response.sendRedirect(request.getContextPath());
     }
-
 }
