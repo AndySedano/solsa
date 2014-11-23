54fbdb76b03bb9af09e614f67dfebe6c9213614f
Andres Sedano <andysedano@gmail.com>
2014-11-23 00:14:23 -0600

    Ya quedo la parte de Cliente/Peticiones

--- SOLSA/src/java/Cliente/Peticiones.java
+++ SOLSA/src/java/Cliente/Peticiones.java
@@ -1,6 +1,16 @@
 package Cliente;
 
+import Beans.Pedido;
+import Beans.Producto;
+import Ventas.EstadoPedido;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +31,33 @@ public class Peticiones extends HttpServlet {
             response.sendRedirect("../Login");
         }
 
+        try (Connection con = Helpers.DB.newConnection(this)) {
+
+            PreparedStatement ps = con.prepareStatement("SELECT Peticion.idPeticion AS id, fecha, estado, Carrito.Usuario_username\n"
+                    + "FROM Peticion INNER JOIN Carrito ON Carrito.idCarrito = Peticion.Carrito_idCarrito\n"
+                    + "Where Carrito.Usuario_username = ?;");
+
+            ps.setString(1, session.getAttribute("username").toString());
+
+            ResultSet rs = ps.executeQuery();
+            
+            ArrayList<Pedido> beans = new ArrayList<>();
+            
+            while (rs.next()) {
+                Pedido bean = new Pedido();
+                bean.setId(rs.getInt("id"));
+                bean.setDate(rs.getDate("fecha").toString());
+                bean.setEstado(rs.getString("estado"));
+                request.setAttribute("bean", bean);
+                beans.add(bean);
+            }
+            
+            request.setAttribute("inf", beans);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EstadoPedido.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         RequestDispatcher disp = getServletContext().getRequestDispatcher("/Cliente/Peticiones.jsp");
         disp.include(request, response);
     }
@@ -28,7 +65,7 @@ public class Peticiones extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
 }
